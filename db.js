60f108a5ec11e71b4859c1da3ff74372becdc33e
raghu957 <raghubomminathan@gmail.com>
2025-01-21 12:39:37 +0530

    Update db.js

--- db.js
+++ db.js
@@ -5,5 +5,23 @@ export const  connectDB = async () =>{
     await mongoose.connect('mongodb+srv://raghuboominathan_99:raghu_2020@cluster0.oipzh.mongodb.net/food-del?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("DB Connected"));
    
 }
+//
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
 
+// dotenv.config(); // Load environment variables from .env file
 
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log("DB Connected");
+//   } catch (error) {
+//     console.error("DB Connection Failed:", error);
+//     process.exit(1); // Exit process with failure
+//   }
+// };
+
+export default connectDB;
