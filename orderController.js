7e1d8840cd42bbd5bb693c75fb0302e89f8c131f
raghu957 <raghubomminathan@gmail.com>
2025-01-21 12:32:07 +0530

    Update orderController.js

--- orderController.js
+++ orderController.js
@@ -1,5 +1,5 @@
-import orderModel from "../models/orderModel.js";
-import userModel from "../models/userModel.js"
+import orderModel from "./models/orderModel.js";
+import userModel from "./models/userModel.js"
 import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
